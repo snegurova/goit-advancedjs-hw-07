7ed822088f44c59a4a9fae0fd6e6955d30dbc2db
innasnegurova <innasnegurova@nxte.nl>
2024-11-25 23:35:41 +0100

    feat: advances js hw 07

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
